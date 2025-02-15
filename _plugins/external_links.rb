4f5bfe77569d72c6f863ad4036521dd611dd0568
Fate Walker <21262207+opabravo@users.noreply.github.com>
2025-02-16 00:20:47 +0800

    Fix domain whitelist bug

--- _plugins/external_links.rb
+++ _plugins/external_links.rb
@@ -3,10 +3,11 @@
     if item.output_ext == ".html"
       content = item.output
       site_url = item.site.config['url']
-      whitelist = ['mailto:', 'tel:']  # whitelist domains
-      # Add rel="nofollow noopener noreferrer" to external anchor tags and ref parameter
-      content.gsub!(%r{<a\s+href="((?!#{whitelist.map { |d| Regexp.escape(d) }.join('|')})[^"]+)"(?![^>]*rel=)}, 
+      whitelist = ['localhost', 'opabravo.github.io'] # Whitelisted prefixes
+
+      content.gsub!(%r{<a\s+href="((?!#{whitelist.map { |d| Regexp.escape(d) }.join('|') })https?:\/\/[^"]+)"(?![^>]*rel=)}, 
                     "<a href=\"\\1?ref=#{site_url.gsub('https://', '')}\" target=\"_blank\" rel=\"nofollow noopener noreferrer\"")
+
       # Update the item content
       item.output = content
     end
